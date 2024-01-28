2470044ea6a4625f2cc09088688aa8726556d786
Dev Planet <mydevplanet@gmail.com>
2024-01-28 22:23:54 +0530

    admin

--- website/admin.py
+++ website/admin.py
@@ -4,16 +4,19 @@ from django.db import models
 
 class AboutAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
-        for field_name in obj._meta.get_fields():
+        # Get all fields in the model
+        for field in obj._meta.get_fields():
             # Check if the field is an ImageField
-            if isinstance(field_name, models.ImageField):
+            if isinstance(field, models.ImageField):
                 # Get the existing instance
                 existing_instance = About.objects.get(pk=obj.pk)
-                # Compare the old image with the new one and delete the old image
-                if getattr(existing_instance, field_name.name) != getattr(obj, field_name.name):
-                    getattr(existing_instance, field_name.name).delete(save=False)
+                
+                # Compare the old image with the new one
+                old_image = getattr(existing_instance, field.name)
+                new_image = getattr(obj, field.name)
 
-        obj.save()
+                # Delete the old image if it has changed
+
 
 # Register your models here.
 admin.site.register(Slide)
