a5d7c323cf4505276df5ad03d827d76c200ba5f1
Dev Planet <mydevplanet@gmail.com>
2024-01-28 22:29:45 +0530

    contd worked on admin

--- website/admin.py
+++ website/admin.py
@@ -4,19 +4,22 @@ from django.db import models
 
 class AboutAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
+        try:
         # Get all fields in the model
-        for field in obj._meta.get_fields():
-            # Check if the field is an ImageField
-            if isinstance(field, models.ImageField):
-                # Get the existing instance
-                existing_instance = About.objects.get(pk=obj.pk)
-                
-                # Compare the old image with the new one
-                old_image = getattr(existing_instance, field.name)
-                new_image = getattr(obj, field.name)
+            for field in obj._meta.get_fields():
+                # Check if the field is an ImageField
+                if isinstance(field, models.ImageField):
+                    # Get the existing instance
+                    existing_instance = About.objects.get(pk=obj.pk)
+                    
+                    # Compare the old image with the new one
+                    old_image = getattr(existing_instance, field.name)
+                    new_image = getattr(obj, field.name)
 
-                # Delete the old image if it has changed
-
+                    # Delete the old image if it has changed
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            raise
 
 # Register your models here.
 admin.site.register(Slide)
